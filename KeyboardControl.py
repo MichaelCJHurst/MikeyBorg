1961d17ff0fc19ed7af910e8aa6b834a463fef81
MichaelHurst93 <mhurst93@live.co.uk>
2016-10-15 15:06:02 +0100

    Started using multithreading

--- KeyboardControl.py
+++ KeyboardControl.py
@@ -2,7 +2,7 @@
 # coding: Latin-1
 #	Configurable variables
 debug = False
-interval = 0.01
+interval = 1
 screenWidth = 512
 screenHeight = 512
 #	Start program
@@ -13,7 +13,7 @@ print("Importing Libraries")
 #   Import libraries
 import PicoBorgRev3 as PicoBorgRev
 import pygame
-#import picamera
+import threading
 import pygame.camera
 from pygame.locals import *
 import io
@@ -45,7 +45,7 @@ print("Setting movement settings")
 timeForward1m = 5.7
 timeSpin360 = 4.8
 #	Power Settings
-voltageIn = 12.0
+voltageIn = 1.6 * 10
 voltageOut = 6.0
 #	setup power limits
 if voltageOut > voltageIn:
@@ -71,175 +71,145 @@ moveRight = False
 moveQuit = False
 streamLength = screenWidth * screenHeight * 3
 black = pygame.Color(0, 0, 0)
-print("Initialising camera")
-#camera = picamera.PiCamera()
-#camera.vflip = False
-#camera.hflip = False
-#camera.brightness = 60
-#camera.resolution = (screenWidth, screenHeight)
-#camera.framerate = 24
-#camera.start_preview()
-#rgb = bytearray(camera.resolution[0] * camera.resolution[1] * 3)
 print("Initialising screen")
 pygame.init()
+print("Initialising camera")
 pygame.camera.init()
+cam = pygame.camera.Camera("/dev/video0", [screenWidth, screenHeight], "RGM")
+cam.start()
+print("Displaying screen")
+image = cam.get_image()
 screen = pygame.display.set_mode([screenWidth, screenHeight])
 pygame.display.set_caption("Press [ESC] to quit")
 screen.fill(black)
 
-print("Defining functions")
-#	Function to handle events
-def PygameHandler(events):
-	#	Variables accessible outside this function
-	global hadEvent
-	global moveUp
-	global moveDown
-	global moveLeft
-	global moveRight
-	global moveQuit
-	#	Handle each event individually
-	for event in events:
-		#	If ESC pressed, quit
-		if event.type == pygame.QUIT:
-			hadEvent = True
-			moveQuit = True
-		#	Else check if this is a key press
-		elif event.type == pygame.KEYDOWN:
-			hadEvent = True
-			if event.key == pygame.K_UP:
-				moveUp = True
-			elif event.key == pygame.K_DOWN:
-				moveDown = True
-			elif event.key == pygame.K_LEFT:
-				moveLeft = True
-			elif event.key == pygame.K_RIGHT:
-				moveRight = True
-			elif event.key == pygame.K_ESCAPE:
-				moveQuit = True
-		#	Else check if this is a key release
-		elif event.type == pygame.KEYUP:
-			hadEvent = True
-			if event.key == pygame.K_UP:
-				moveUp = False
-			elif event.key == pygame.K_DOWN:
-				moveDown = False
-			elif event.key == pygame.K_LEFT:
-				moveLeft = False
-			elif event.key == pygame.K_RIGHT:
-				moveRight = False
-			elif event.key == pygame.K_ESCAPE:
-				moveQuit = False
+print("Defining Camera class")
+class CameraClass(threading.Thread):
+	def __init__(self, threadID, name, counter):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.counter = counter
+	
+	def run(self):
+		while True:
+			#	If an image is ready, use it
+			if cam.query_image():
+				image = cam.get_image()
+				screen.blit(image, [0, 0])
+				#	Update background with the image
+				pygame.display.update()
+				time.sleep(interval)
 
-#	Testing Movement
-def TestMove(driveLeft, driveRight, numSeconds):
-	#	set the motors running
-	PBR.SetMotor1(driveRight * maxPower)
-	PBR.SetMotor2(-driveLeft * maxPower)
-	#	wait for the time specified
-	time.sleep(numSeconds)
-	#	turn off the motors
-	PBR.MotorsOff()
-#	Setting speeds
+print("Defining Event class")
+class EventClass(threading.Thread):
+	def __init__(self, threadID, name, counter):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.counter = counter
+	
+	def run(self):
+		while True:
+		#	Variables accessible outside this function
+			global hadEvent
+			global moveUp
+			global moveDown
+			global moveLeft
+			global moveRight
+			global moveQuit
+			events = pygame.event.get()
+			#	Handle each event individually
+			for event in events:
+				#	If ESC pressed, quit
+				if event.type == pygame.QUIT:
+					hadEvent = True
+					moveQuit = True
+				#	Else check if this is a key press
+				elif event.type == pygame.KEYDOWN:
+					hadEvent = True
+					if event.key == pygame.K_UP:
+						moveUp = True
+					elif event.key == pygame.K_DOWN:
+						moveDown = True
+					elif event.key == pygame.K_LEFT:
+						moveLeft = True
+					elif event.key == pygame.K_RIGHT:
+						moveRight = True
+					elif event.key == pygame.K_ESCAPE:
+						moveQuit = True
+				#	Else check if this is a key release
+				elif event.type == pygame.KEYUP:
+					hadEvent = True
+					if event.key == pygame.K_UP:
+						moveUp = False
+					elif event.key == pygame.K_DOWN:
+						moveDown = False
+					elif event.key == pygame.K_LEFT:
+						moveLeft = False
+					elif event.key == pygame.K_RIGHT:
+						moveRight = False
+					elif event.key == pygame.K_ESCAPE:
+						moveQuit = False
+			#	If something has changed since last iteration, do something
+			if hadEvent:
+				hadEvent = False
+				#	If ESC was clicked
+				if moveQuit:
+					print("[ESC] clicked, terminating program")
+					PBR.MotorsOff()
+					print("Program Finished")
+					print("======================================")
+					pygame.quit()
+					exit()
+				#	If going forwards
+				elif moveUp:
+					#	If also going left
+					if moveLeft == True and moveRight == False:
+						SetSpeed(0.2, 1.0)
+					#	If also going right
+					elif moveLeft == False and moveRight == True:
+						SetSpeed(1.0, 0.2)
+					#	If just going forward
+					else:
+						SetSpeed(1.0, 1.0)
+				#	If going backwards
+				elif moveDown:
+					#	If also going left
+					if moveLeft == True and moveRight == False:
+						SetSpeed(-1, -0.2)
+					#	If also going right
+					elif moveLeft == False and moveRight == True:
+						SetSpeed(-0.2, -1)
+					#	If just going backwards
+					else:
+						SetSpeed(-1, -1)
+				#	If going left
+				elif moveLeft:
+					#	Turn left, if not also going right
+					if moveRight == False:
+						SetSpeed(-1.0, 1.0)
+				#	If going right
+				elif moveRight:
+					SetSpeed(1.0, -1.0)
+				else:
+					PBR.MotorsOff()
+
 def SetSpeed(driveLeft, driveRight):
 	PBR.SetMotor1(driveRight * maxPower)
 	PBR.SetMotor2(-driveLeft * maxPower)
-#	If debugging, run tests
-if debug == True:
-	print("======================================")
-	print("Initialisation complete, running tests")
-	print("======================================")
-	print("Testing moving forwards:")
-	TestMove(+1.0, +1.0, 2)
-	time.sleep(0.25)
-	print("Testing moving backwards:")
-	TestMove(-1.0, -1.0, 2)
-	time.sleep(0.25)
-	print("Testing moving left:")
-	TestMove(+1.0, -1.0, 2)
-	time.sleep(0.25)
-	print("Testing moving right:")
-	TestMove(-1.0, +1.0, 2)
-	time.sleep(0.25)
-	print("======================================")
-	print("Testing complete, awaiting input")
-	print("Press [ESC] to quit")
-	print("======================================")
-#	If not debugging, just wait for input
-else:
-	print("======================================")
-	print("Initialisation complete, awaiting input")
-	print("Press [ESC] to quit")
-	print("======================================")
-cam = pygame.camera.Camera("/dev/video0", [screenWidth, screenHeight], "RGM")
-cam.start()
-image = cam.get_image()
-try:
-	#	Loop forevermore, unless ESC pressed
-	while True:
-		#	Camera stuff
-		#       If image is ready, use it
-		if cam.query_image():
-                        image = cam.get_image()
-                        #       Set image as background
-                        screen.blit(image, [0, 0])
-		#stream = io.BytesIO()
-		#camera.capture(stream, use_video_port=True, format="rgb", resize=(screenWidth, screenHeight))
-		#stream.seek(0)
-		#stream.readinto(rgb)
-		#stream.close()
 
-		#img = pygame.image.frombuffer(stream, [screenWidth, screenHeight], "RGB")
-		#stream.close()
-		#img = pygame.image.frombuffer(rgb[0:(streamLength)], [screenWidth, screenHeight], "RGB")
-		#screen.blit(img, (0, 0))
-		pygame.display.update()
-		#	Get the currently pressed keys
-		PygameHandler(pygame.event.get())
-		#	If something has changed since last iteration, do something
-		if hadEvent:
-			hadEvent = False
-			#	If ESC was clicked
-			if moveQuit:
-				print("[ESC] clicked, terminating program")
-				break
-			#	If going forwards
-			elif moveUp:
-				#	If also going left
-				if moveLeft == True and moveRight == False:
-					SetSpeed(0.2, 1.0)
-				#	If also going right
-				elif moveLeft == False and moveRight == True:
-					SetSpeed(1.0, 0.2)
-				#	If just going forward
-				else:
-					SetSpeed(1.0, 1.0)
-			#	If going backwards
-			elif moveDown:
-				#	If also going left
-				if moveLeft == True and moveRight == False:
-					SetSpeed(-1, -0.2)
-				#	If also going right
-				elif moveLeft == False and moveRight == True:
-					SetSpeed(-0.2, -1)
-				#	If just going backwards
-				else:
-					SetSpeed(-1, -1)
-			#	If going left
-			elif moveLeft:
-				#	Turn left, if not also going right
-				if moveRight == False:
-					SetSpeed(-1.0, 1.0)
-			#	If going right
-			elif moveRight:
-				SetSpeed(1.0, -1.0)
-			else:
-				PBR.MotorsOff()
-		#	Wait for the interval
-		#time.sleep(interval)
-	#	Stop the motors
-	PBR.MotorsOff()
-#	CTRL+C pressed, so quit
-except KeyboardInterrupt:
-	PBR.MotorsOff()
-print("Program Finished")
 print("======================================")
+print("Initialisation complete, awaiting input")
+print("Press [ESC] to quit")
+print("======================================")
+
+cameraThread = CameraClass(1, "Camera", 1)
+eventThread  = EventClass(2,  "Event",  1)
+
+cameraThread.start()
+eventThread.start()
+
+#PBR.MotorsOff()
+#print("Program Finished")
+#print("======================================")
