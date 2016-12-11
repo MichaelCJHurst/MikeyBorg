4fd6a444f09d04b3bffa97f4e72546390b55a30f
MichaelHurst93 <mhurst93@live.co.uk>
2016-12-11 16:52:22 +0000

    Added a working console on the left

--- Classes/MikeyBorgUIClass.py
+++ Classes/MikeyBorgUIClass.py
@@ -5,12 +5,15 @@ import pygame
 #	Set the colours
 backgroundColour  = pygame.Color(119, 136, 153)
 blackColour       = pygame.Color(0,   0,   0)
-boxColour         = pygame.Color(0,   0,   0)
+boxColour         = pygame.Color(205, 201, 201)
+consoleColour     = pygame.Color(0,   0,   0)
 noImageColour     = pygame.Color(0,   0,   0)
+#	Set the colours for fonts
+consoleFontColour = (50, 205, 50)
 
 class MikeyBorgUI:
 		
-	def __init__(self, screenSize, imageSize):
+	def __init__(self, screenSize, imageSize, consoleSize):
 		self.border          = 5
 		self.screenSize      = screenSize
 		self.imageSize       = [imageSize[0] * 2, imageSize[1] * 2]
@@ -19,6 +22,10 @@ class MikeyBorgUI:
 		self.consoleSize     = [screenSize[0] - self.imageSize[0] - (self.border * 3), self.screenSize[1] - (self.border * 2)]
 		self.infoPosition    = [self.imagePosition[0], (self.border * 2) + self.imageSize[1]]
 		self.infoSize        = [self.imageSize[0], self.screenSize[1] - self.imageSize[1] - (self.border * 3)]
+		self.consoleRows     = consoleSize[1]
+		self.consoleLength   = consoleSize[0]
+		self.consoleChanged  = False
+		self.consoleList     = []
 		#	Initialize pygame, and set the page title
 		pygame.init()
 		pygame.display.set_caption("MikeyBorg")
@@ -27,11 +34,56 @@ class MikeyBorgUI:
 		self.screen.fill(backgroundColour)
 		#	Set up the font
 		self.font = pygame.font.SysFont("monospace", 15)
+		#self.text = self.font.render("Hi", True, (255, 0, 0), (255, 255, 255))
 		#	Set up the image, console and info boxes
-		pygame.draw.rect(self.screen, noImageColour, [self.imagePosition[0],   self.imagePosition[1],   self.imageSize[0],   self.imageSize[1]])
-		pygame.draw.rect(self.screen, boxColour,     [self.consolePosition[0], self.consolePosition[1], self.consoleSize[0], self.consoleSize[1]])
-		pygame.draw.rect(self.screen, boxColour,     [self.infoPosition[0],    self.infoPosition[1],    self.infoSize[0],    self.infoSize[1]])
+		self.drawImageBorder()
+		self.drawConsole()
+		self.drawInfoBox()
 		pygame.display.update()
+		return
+	
+	def drawImageBorder(self):
+		pygame.draw.rect(self.screen, noImageColour, [self.imagePosition[0],   self.imagePosition[1],   self.imageSize[0],   self.imageSize[1]])
+		return
+	
+	def drawConsole(self):
+		pygame.draw.rect(self.screen, consoleColour, [self.consolePosition[0], self.consolePosition[1], self.consoleSize[0], self.consoleSize[1]])
+		return
+	
+	def drawInfoBox(self):
+		pygame.draw.rect(self.screen, boxColour,     [self.infoPosition[0],    self.infoPosition[1],    self.infoSize[0],    self.infoSize[1]])
+		return
+
+	def _displayConsole(self):
+		self.consoleChanged  = False
+		textLocation = [self.consolePosition[0] + self.border, self.consolePosition[1] + self.border]
+		#	Only show the last x rows
+		self.consoleList = self.consoleList[-self.consoleRows:]
+		#	Clear the console
+		self.drawConsole()
+		#	Check the length
+		i = 0
+		for consoleLine in self.consoleList:
+			if len(consoleLine) > self.consoleLength:
+				self.consoleList.insert(i + 1, consoleLine[-self.consoleLength:])
+				self.consoleList[i] = consoleLine[:self.consoleLength]
+			i += 1
+		#	Only show the last x rows
+		self.consoleList = self.consoleList[-self.consoleRows:]
+		#	Output it
+		for consoleLine in self.consoleList:
+			text = self.font.render(consoleLine, True, consoleFontColour)
+			self.screen.blit(text, textLocation)
+			textLocation[1] += 20
+		return
+
+	def addToConsole(self, add):
+		self.consoleList.append(add)
+		self._displayConsole()
+		return
+	
+	def addFullWidthToConsole(self, add):
+		self.addToConsole(add * self.consoleLength)
 		return
 
 	def updateImage(self, image):
--- MikeyBorg.py
+++ MikeyBorg.py
@@ -12,24 +12,26 @@ from   multiprocessing import Process, Value
 #	Read the settings from the config file
 config = SafeConfigParser()
 config.read("MikeyBorg.ini")
-screenWidth   = int(config.get("screen", "width"))
-screenHeight  = int(config.get("screen", "height"))
-imageWidth    = int(config.get("image",  "width"))
-imageHeight   = int(config.get("image",  "height"))
-imagePath     = config.get("image", "path")
+screenWidth   = int(config.get("screen",  "width"))
+screenHeight  = int(config.get("screen",  "height"))
+imageWidth    = int(config.get("image",   "width"))
+imageHeight   = int(config.get("image",   "height"))
+imagePath     =     config.get("image",   "path")
+consoleWidth  = int(config.get("console", "width"))
+consoleHeight = int(config.get("console", "height"))
 #	Variables defined using above configurable variables
 displayWidth  = imageWidth  * 2
 displayHeight = imageHeight * 2
 imageX        = (screenWidth - displayWidth) / 2
 imageY        = 20
 #	Initialize the stuff
-MikeyBorgUI = MikeyBorgUIClass.MikeyBorgUI([screenWidth, screenHeight], [imageWidth, imageHeight])
+MikeyBorgUI = MikeyBorgUIClass.MikeyBorgUI([screenWidth, screenHeight], [imageWidth, imageHeight], [consoleWidth, consoleHeight])
 MikeyCam  = MikeyCamClass.MikeyCam([imageWidth, imageHeight])
 MikeyBorg = MikeyBorgClass.MikeyBorg()
 closeNow  = Value("b", 0)
 
 #	Function used to update the display
-def displayLoop(MikeyCam, close):
+def cameraLoop(close, MikeyBorgUI, MikeyCam):
 		try:
 			while True:
 				image = MikeyCam.getImage([displayWidth, displayHeight])
@@ -41,26 +43,41 @@ def displayLoop(MikeyCam, close):
 		except KeyboardInterrupt:
 			close.value = 1
 			MikeyCam.stop()
+
+#	Function used to display the controls in the console
+def showControls():
+	MikeyBorgUI.addFullWidthToConsole("=")
+	MikeyBorgUI.addToConsole("MikeyBorg Version 1.0.0")
+	MikeyBorgUI.addFullWidthToConsole("=")
+	MikeyBorgUI.addToConsole("Press 'W' to go forward")
+	MikeyBorgUI.addToConsole("Press 'A' to turn left")
+	MikeyBorgUI.addToConsole("Press 'D' to turn right")
+	MikeyBorgUI.addToConsole("Press 'S' to go backwards")
+	MikeyBorgUI.addToConsole("Press '[SHIFT]' to go slower")
+	MikeyBorgUI.addToConsole("Press '[ESC]' to quit")
+	MikeyBorgUI.addFullWidthToConsole("=")
 		
 #	Function to manage the inputs and movement
-def inputLoop(MikeyBorg, close):
+def inputLoop(close, MikeyBorg):
 	inputs = InputsClass.Inputs()
 	while True:
 		inputs.readInputs(pygame.event.get(), close)
 		if close.value == 1:
 			break
-		if inputs.hadEvent:
+		elif inputs.hadEvent:
 			inputs.manageInputs(MikeyBorg)
 	MikeyBorg.motorsOff()
 
-displayProcess = Process(target=displayLoop, args=(MikeyCam,  closeNow,))
-inputProcess   = Process(target=inputLoop,   args=(MikeyBorg, closeNow,))
-displayProcess.daemon = True
+showControls()
+
+cameraProcess         = Process(target=cameraLoop,  args=(closeNow, MikeyBorgUI, MikeyCam,))
+inputProcess          = Process(target=inputLoop,   args=(closeNow, MikeyBorg,))
+cameraProcess.daemon  = True
 inputProcess.daemon   = True
-displayProcess.start()
+cameraProcess.start()
 inputProcess.start()
 
-displayProcess.join()
+cameraProcess.join()
 inputProcess.join()
 print("Program Finished")
 print("======================================")
